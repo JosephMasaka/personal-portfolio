2dcab77cf1f6c3f0b02360cd6e808658cdc84c3e
Joss <34286057+JosephMasaka@users.noreply.github.com>
2022-11-09 12:42:25 +0300

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -61,7 +61,7 @@ export const Projects = () => {
                       <Nav.Link eventKey="second">JavaScipt</Nav.Link>
                     </Nav.Item>
                     <Nav.Item>
-                      <Nav.Link eventKey="third">HTML & CSS</Nav.Link>
+                      <Nav.Link eventKey="third">HTML <span className="hidden-on">& CSS</span></Nav.Link>
                     </Nav.Item>
                   </Nav>
                   <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
